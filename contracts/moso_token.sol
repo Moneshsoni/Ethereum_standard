2cc092e4e45832d4a67c2772aed93db8948602da
Moneshsoni <sonimonesh115@gmail.com>
2022-12-10 17:24:38 +0530

    add the mint and burn

--- contracts/moso_token.sol
+++ contracts/moso_token.sol
@@ -1,10 +1,21 @@
 // contracts/MosoToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
-
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract MosoToken is ERC20 {
+contract MosoToken is ERC20 ,Ownable{
     constructor(uint256 initialSupply) public ERC20("MOSO", "MS") {
         _mint(msg.sender, initialSupply);
     }
+    
+    function mint(address account, uint256 amount)public onlyOwner{
+        _mint(account, amount);
+    }
+    
+    
+    function burn(address account,uint256 amount)public onlyOwner{
+        _burn(account,amount);
+    }
+    
+    
 }
