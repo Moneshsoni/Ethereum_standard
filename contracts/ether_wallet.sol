c7557768b43db3cf4b4da3df0421422fb8c07bde
Moneshsoni <sonimonesh115@gmail.com>
2022-12-17 18:55:52 +0530

    add the saturday work multi_signature

---
+++ contracts/ether_wallet.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.8.9;
+contract EtherWallet{
+    address payable public owner;
+    constructor(){
+        owner = payable(msg.sender);
+    }
+    
+    receive() external payable {}
+    
+    function withdraw(uint _amount)external{
+        require(msg.sender == owner,"Caller is not owner");
+        payable(msg.sender).transfer(_amount);
+    }
+    
+    function getBalance() external view returns(uint){
+        return address(this).balance;
+    }
+}
