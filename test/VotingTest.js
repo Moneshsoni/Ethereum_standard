6e19906519e2988eb622c22c269f1af42766a121
Moneshsoni <sonimonesh115@gmail.com>
2022-12-25 00:23:27 +0530

    add the latest code

--- test/VotingTest.js
+++ test/VotingTest.js
@@ -1,16 +1,20 @@
 const { expect } = require('chai');
-const {hre, ethers} = require('hardhat');
+const {hre, ethers} = require("hardhat");
 const { etherscan } = require('../hardhat.config');
 // Start test of Voting Project
 describe("Voter Machine Project",()=>{
     before(async ()=>{
-        const signers = await ethers.getSigner();
-       
+        signers = await ethers.getSigner();
+        Lock = await hre.ethers.getContractFactory("Ballot_Vote");
+        lock = await Lock.deploy();
+        await lock.deployed();
+        console.log(">>>",lock.address);
+  
     })
     
     describe("Test Cases for Voting Project", ()=>{
         it("Should check test",async () =>{
-            console.log("????",signers);
+            console.log("????",lock);
         })
     })
 })
