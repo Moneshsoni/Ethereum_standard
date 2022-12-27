613e582990a62c888490dc4f9f0459259b05dc6f
Moneshsoni <sonimonesh115@gmail.com>
2022-12-27 09:01:50 +0530

    add the token test cases

--- contracts/moso_token.sol
+++ contracts/moso_token.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.17;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract MosoToken is ERC20 ,Ownable{
+contract MyToken is ERC20 ,Ownable{
     constructor(uint256 initialSupply) public ERC20("MOSO", "MS") {
         _mint(msg.sender, initialSupply);
     }
